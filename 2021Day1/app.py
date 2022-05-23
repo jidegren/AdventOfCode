762fac20b2b7814845a675881222cc09fd541f54
Jidegren <RJIDEGRE@volvocars.com>
2022-05-23 15:08:15 +0200

    saved all data in text file

--- 2021Day1/app.py
+++ 2021Day1/app.py
@@ -1,1 +1,15 @@
-print("Hello")
+with open('C:/Github/AdventOfCode/2021Day1/messureData.txt') as f:
+    lines = f.readlines()
+    f.close()
+
+line_without_newline = []
+
+for line in lines:
+    line = line.rstrip()
+    line_without_newline.append(line)
+
+
+print(line_without_newline)
+
+    
+    
